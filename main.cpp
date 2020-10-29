f5378443323c7308bf068a68fa8ba478b27146bf
Edgar Omar Monreal <edgar.monreal7686@alumnos.udg.mx>
2020-10-28 21:09:31 -0600

    Se realizó con exito el programa

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "arreglo.h"
+
+int main() {
+  Arreglo arreglo;
+  
+  arreglo.insertar_final("Dark Souls");
+  arreglo.insertar_final("Pokemon");
+  arreglo.insertar_final("Neptunia");
+  arreglo.insertar_final("Spoore");
+  arreglo.insertar_final("Fire Emblem");
+  arreglo.insertar_final("Final Fantasy");
+  arreglo.insertar_final("FFBE");
+  arreglo.insertar_final("Fate GO");
+  
+  for (size_t i = 0; i < arreglo.size(); i++)
+  {
+    cout << arreglo[i] << "   ";
+  }
+
+  cout << endl;
+  arreglo.insertar_inicio("Mahouka");
+  arreglo.insertar_inicio("Danmachi");
+
+  for (size_t i = 0; i < arreglo.size(); i++)
+  {
+    cout << arreglo[i] << "   ";
+  }
+}
